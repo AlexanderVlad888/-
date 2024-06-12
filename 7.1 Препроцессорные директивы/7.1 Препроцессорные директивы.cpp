600afa6eda9eb8a8fbe37dfa5d58c63deb544b87
Alexander <str_999@bk.ru>
2024-06-12 13:14:07 +0500

    update 7.2

---
+++ 7.1 Препроцессорные директивы/7.1 Препроцессорные директивы.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+#define MODE 15
+
+
+
+#ifdef MODE
+int add(int a, int b)
+{
+    return a + b;
+}
+
+#else 
+#error MODE is not defined
+
+#endif
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+#if MODE == 1
+    int a, b;
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Сумма чисел: " << add(a, b) << std::endl;
+
+
+#elif MODE == 0
+
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+
+#else 
+    std::cout << "Неизвестный режим" << std::endl;
+
+#endif
+
+}
